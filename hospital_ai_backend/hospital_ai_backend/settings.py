7b10406ba6323afac027a359ee041d90243aaeb2
Rey-24-rey <rainbowsogiesc.community@gmail.com>
2025-02-22 17:50:57 +0300

    corsheaders

--- hospital_ai_backend/hospital_ai_backend/settings.py
+++ hospital_ai_backend/hospital_ai_backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
+    'corsheaders',
     
     'resources',
 ]
@@ -51,8 +52,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 ROOT_URLCONF = 'hospital_ai_backend.urls'
 
 TEMPLATES = [
